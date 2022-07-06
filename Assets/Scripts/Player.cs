51fc364085b0e310b3d50542e7e796e68155e6eb
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2022-07-05 19:03:44 -0500

    Adding player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed;
 
-    // Update is called once per frame
+    public Rigidbody rig;
+
     void Update()
     {
-        
+        Move();
     }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 dir = transform.right * x + transform.forward * z;
+        dir *= moveSpeed;
+        dir.y = rig.velocity.y;
+
+        rig.velocity = dir;
+
+    }
+
 }

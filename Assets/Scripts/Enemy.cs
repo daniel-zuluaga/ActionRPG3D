a8a83108eff57fd38c5ca671917234219cbae211
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2022-07-07 18:01:07 -0500

    To the enemy I add the functions you need to chase the player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Player player;
+    public float AttackDistance;
+    public int damage;
+    public int health;
 
-    // Update is called once per frame
+    private bool isAttacking;
+    private bool isDead;
+
+    public NavMeshAgent agent;
+
     void Update()
     {
-        
+        if (isDead)
+            return;
+
+        if(Vector3.Distance(transform.position, player.transform.position) <= AttackDistance)
+        {
+            agent.isStopped = false;
+            // Attack code
+
+        }
+        else
+        {
+            agent.isStopped = false;
+            agent.SetDestination(player.transform.position);
+        }
+
     }
+
 }
